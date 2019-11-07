c69b50436e56a4920d6a620c576c284563bd56ec
Anshul Ahuja <anshul.ahu@gmail.com>
2019-11-07 19:15:24 +0530

    Add str bit conversion methods to utils

--- utils.py
+++ utils.py
@@ -30,3 +30,22 @@ def mod2div(divident, divisor):
 
     checkword = tmp
     return checkword
+
+
+def str2bits(string):
+    res = ""
+    for x in string:
+        bits = ''.join(format(ord(x), 'b'))
+        while len(bits) != 8:
+            bits = '0' + bits
+        res += str(bits)
+    return res
+
+
+def bits2str(string):
+    data = ""
+    for i in range(0, len(string), 8):
+        j = min(i + 8, len(string))
+        char = chr(int(string[i:j], 2))
+        data += char
+    return data

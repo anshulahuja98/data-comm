8713c4be487aa790209ad55fb38a9fe30fc12730
Anshul Ahuja <anshul.ahu@gmail.com>
2019-11-11 15:09:21 +0530

    Persist client connection

--- README.md
+++ README.md
@@ -10,3 +10,11 @@ Implemented in python using socket programming.
 Team:
 - Anshul Ahuja (B17CS006)
 - Chakshu Gupta (B17CS061)
+
+
+### Steps to use
+To run the server
+```python3 server.py```
+
+To run the client
+```python3 client.py```
--- client.py
+++ client.py
@@ -7,30 +7,31 @@ from error import induce_error
 
 
 def main():
-    sock = socket.socket()
-    sock.connect(('127.0.0.1', SERVER_PORT))
+    inp = ""
+    while inp != 'qq':
+        sock = socket.socket()
+        sock.connect(('127.0.0.1', SERVER_PORT))
+        inp = input("Enter data to send: ")
+        data = str2bits(inp)
+        print("Data : " + inp)
+        encoded_data = crc_encode(data, CRC_KEY)
+        print("Encoded data: " + encoded_data)
+        op = input("Induce error in data to be sent? (y/n) ")
+        if op == "y":
+            encoded_data = induce_error(encoded_data)
+        frame = create_frame(encoded_data)
+        print("Frame sent: " + frame)
+        physical_enc_data = manchester_encode(frame)
+        print("Manchester encoded frame: " + physical_enc_data)
 
-    inp = input("Enter data to send: ")
-    data = str2bits(inp)
-    print("Data : " + inp)
-    encoded_data = crc_encode(data, CRC_KEY)
-    print("Encoded data: " + encoded_data)
-    op = input("Induce error in data to be sent? (y/n) ")
-    if op == "y":
-        encoded_data = induce_error(encoded_data)
-    frame = create_frame(encoded_data)
-    print("Frame sent: " + frame)
-    physical_enc_data = manchester_encode(frame)
-    print("Manchester encoded frame: " + physical_enc_data)
+        sock.sendall(physical_enc_data.encode())
 
-    sock.sendall(physical_enc_data.encode())
+        response = sock.recv(BUFFER_SIZE)
+        response = response.decode()
+        print("###### RESPONSE RECIEVED FROM SERVER ######")
+        print(response)
 
-    response = sock.recv(BUFFER_SIZE)
-    response = response.decode()
-    print("###### RESPONSE RECIEVED FROM SERVER ######")
-    print(response)
-
-    sock.close()
+        sock.close()
 
 
 if __name__ == '__main__':
